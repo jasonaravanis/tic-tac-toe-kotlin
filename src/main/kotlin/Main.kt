9ec034ff7b48449b8600a676df477ee509393d56
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2025-02-08 09:27:03 +1100

    feat: implement convertBoardStringToMatrix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,86 @@
 package tictactoe
 
-fun main() {
-    val input = readln()
+fun convertBoardStringToMatrix(board: String): MutableList<MutableList<Char>> {
+    val matrix =
+        mutableListOf(
+            mutableListOf<Char>('_', '_', '_'),
+            mutableListOf<Char>('_', '_', '_'),
+            mutableListOf<Char>('_', '_', '_'),
+        )
+
+    for (i in board.indices) {
+        var row: Int
+
+        if (i in 0..2) {
+            row = 0
+        } else if (i in 3..5) {
+            row = 1
+        } else {
+            row = 2
+        }
+
+        val column = i % 3
+
+        matrix[row][column] = board[i]
+    }
+
+    return matrix
+}
+
+fun printBoard(boardMatrix: MutableList<MutableList<Char>>) {
     print(
         """
         ---------
-        | ${input[0]} ${input[1]} ${input[2]} |
-        | ${input[3]} ${input[4]} ${input[5]} |
-        | ${input[6]} ${input[7]} ${input[8]} |
+        | ${boardMatrix[0][0]} ${boardMatrix[0][1]} ${boardMatrix[0][2]} |
+        | ${boardMatrix[1][0]} ${boardMatrix[1][1]} ${boardMatrix[1][2]} |
+        | ${boardMatrix[2][0]} ${boardMatrix[2][1]} ${boardMatrix[2][2]} |
         ---------
         """.trimIndent(),
     )
+}
+
+enum class Player(
+    val token: Char,
+) {
+    X('X'),
+    O('O'),
+}
+
+fun isWinByPlayer(
+    boardMatrix: MutableList<MutableList<Char>>,
+    player: Player,
+): Boolean {
+    fun isRowWin(): Boolean {
+        var isWin = false
+        for (row in boardMatrix) {
+            if (row.all { it == player.token }) {
+                isWin = true
+            }
+        }
+        return isWin
+    }
+
+//    fun isColumnWin(): Boolean {
+//    }
+//
+//    fun isDiagonalWin(): Boolean {
+//    }
+
+//    return isRowWin() || isColumnWin() || isDiagonalWin()
+    return isRowWin()
+}
+
+fun getGameStateFromMatrix(boardMatrix: MutableList<MutableList<Char>>) {
+}
+
+fun main() {
+    val input = readln()
+
+    val boardMatrix = convertBoardStringToMatrix(input)
+
+    printBoard(boardMatrix)
+
+    val isRowWinByX = isWinByPlayer(boardMatrix, Player.X)
+
+    println("Is row win by X: $isRowWinByX")
 }
